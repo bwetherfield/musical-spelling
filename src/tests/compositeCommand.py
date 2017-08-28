53937754ffc8d10c2ee44382f604f8aec324f1b3
Ben Wetherfield <bwetherfield@gmail.com>
2017-08-28 13:50:28 -0400

    refactor factor unionTable formation out of tests

--- src/tests/compositeCommand.py
+++ src/tests/compositeCommand.py
@@ -6,13 +6,13 @@ import sqlite3
 
 class TestCompositeCommand(unittest.TestCase):
 
-    """CompositeCommand test class
-
-    Uses non-oo implementation of db to test the oo-style commands
-    """
+    """CompositeCommand test class"""
 
     def setUp(self):
-        pass
+        self.myDb = Db('unionDb')
+        self.myDb['unionTable'] = dict(id = 'INTEGER')
+        rowInsert = Insert('unionTable', id=1)
+        self.myDb.execute(rowInsert)
 
     def tearDown(self):
         pass
@@ -21,12 +21,8 @@ class TestCompositeCommand(unittest.TestCase):
         self.assertNotIsInstance(Union(), Command)
 
     def test_unionOneSelect(self):
-        self.myDb = Db('unionDb')
-        self.myDb['unionTable'] = dict(id = 'INTEGER')
-        rowInsert = Insert('unionTable', id=1)
         oneSelect = Select('unionTable', "id == 1")
         unionOneSelect = Union(oneSelect)
-        self.myDb.execute(rowInsert)
         rows = self.myDb.execute(unionOneSelect)
         self.assertIsNotNone(rows)
 
