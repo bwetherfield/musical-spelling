34fb25f757309f7a42bc0f4bf17657a856ce13c5
Ben Wetherfield <bwetherfield@gmail.com>
2017-08-30 12:16:43 -0400

    pass test basic chord entry building
    just id added to database

--- src/musicspell/noteBuilder.py
+++ src/musicspell/noteBuilder.py
@@ -1,16 +1,28 @@
 from musicspell.command import Insert
 
 class NoteBuilder:
-    def __init__(self, tbl,  mn):
+    def __init__(self, tbl,  m21n):
         self._tbl = tbl
-        self._mn = mn
+        self._m21n = m21n
         self._kw = {}
 
     def getEntry(self):
         return Insert(self._tbl, self._kw)
 
     def build_id(self):
-        self._kw['id'] = self._mn.id
+        self._kw['id'] = self._m21n.id
+
+class ChordBuilder:
+    def __init__(self, tbl,  m21c):
+        self._tbl = tbl
+        self._m21c = m21c
+        self._kw = {}
+
+    def getEntry(self):
+        return Insert(self._tbl, self._kw)
+
+    def build_id(self):
+        self._kw['id'] = self._m21c.id
 
 class DataScore:
     def buildEntry(self, builder):
--- src/tests/noteBuilder.py
+++ src/tests/noteBuilder.py
@@ -2,6 +2,7 @@ from musicspell.noteBuilder import *
 from musicspell.command import Insert
 import unittest
 import music21.note as mn
+import music21.chord as mc
 import sqlite3
 
 class TestBuilder(unittest.TestCase):
@@ -22,11 +23,11 @@ class TestBuilder(unittest.TestCase):
         self.assertIsInstance(noteInsert, Insert)
 
     def test_chordBuilder(self):
-        chordBuilder = ChordBuilder('table', mn.Chord())
+        chordBuilder = ChordBuilder('table', mc.Chord())
         score = DataScore()
         score.buildEntry(chordBuilder)
         chordInsert = chordBuilder.getEntry()
-        self.assertIsInstance(noteInsert, Insert)
+        self.assertIsInstance(chordInsert, Insert)
 
 if __name__ == "__main__":
     unittest.main()
