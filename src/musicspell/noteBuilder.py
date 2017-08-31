6852e8e74b802649f1765f8e5ab76e3e76c4186b
Ben Wetherfield <bwetherfield@gmail.com>
2017-08-31 09:32:54 -0400

    add bare docstrings to noteBuilder.py
    reSt style

--- src/musicspell/noteBuilder.py
+++ src/musicspell/noteBuilder.py
@@ -3,16 +3,22 @@ from musicspell.compositeCommand import ManyCommand
 
 class AbstractBuilder:
     """abstract database-entry builder
-
+    
     unimplemented methods do not raise exceptions so as to allow inheriting
-    classes to pick and choose which parts to build"""
+    classes to pick and choose which parts to build
+
+
+    """
 
     def build_id(self): pass
 
 class NoteBuilder(AbstractBuilder):
-    """note database-entry builder
+        """note database-entry builder
+        
+            use to build a row entry from a music21 note
 
-    use to build a row entry from a music21 note"""
+
+        """
 
     def __init__(self, tbl,  m21n):
         self._tbl = tbl
@@ -20,15 +26,20 @@ class NoteBuilder(AbstractBuilder):
         self._kw = {}
 
     def getEntry(self):
+        """ """
         return Insert(self._tbl, self._kw)
 
     def build_id(self):
+        """ """
         self._kw['id'] = self._m21n.id
 
 class ChordBuilder(AbstractBuilder):
     """chord database-entry builder
+    
+    use to build a row entry from a music21 chord
 
-    use to build a row entry from a music21 chord"""
+
+    """
 
     def __init__(self, tbl,  m21c):
         self._tbl = tbl
@@ -36,16 +47,26 @@ class ChordBuilder(AbstractBuilder):
         self._kw = {}
 
     def getEntry(self):
+        """ """
         idInsert = Insert(self._tbl, self._kw)
         return ManyCommand(idInsert)
 
     def build_id(self):
+        """ """
         self._kw['id'] = self._m21c.id
 
 class Director:
     """Directs builders
+    
+    calls each build method for a specific builder
 
-    calls each build method for a specific builder"""
+
+    """
 
     def buildEntry(self, builder):
+        """
+
+        :param builder: 
+
+        """
         builder.build_id()
