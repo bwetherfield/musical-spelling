69a4204a3d2c402292ed90512f43e17d2e6e0d2c
Ben Wetherfield <bwetherfield@gmail.com>
2017-08-31 19:33:28 -0400

    add module and class level docstrings to dataScore module
    google style docstrings

--- src/musicspell/dataScore.py
+++ src/musicspell/dataScore.py
@@ -1,9 +1,26 @@
+"""Music score object that uses database and entry-builders
+
+Todo:
+    * Add method docstrings
+
+"""
 from musicspell.noteBuilder import Director, NoteBuilder, ChordBuilder
 from musicspell.db import Db
 import music21.note
 
 class DataScore:
-    """(musical) score abstraction on top of database"""
+    """(musical) score abstraction on top of database
+
+    Arguments:
+        name (str): name of .db file without .db suffix (holds database).
+            Defaults to 'defaultScore'
+
+    Attributes:
+        director (:class:`musicspell.noteBuilder.Director): directs row
+            creation
+        db: :mod:`sqlite3` database which stores note from musical score
+
+        """
 
     def __init__(self, name='defaultScore'):
         self.director = Director()
@@ -12,8 +29,11 @@ class DataScore:
     def accept(self, tbl, scoreObj):
         """
 
-        :param tbl: 
-        :param scoreObj: 
+        Args:
+          tbl: param scoreObj:
+          scoreObj: 
+
+        Returns:
 
         """
         if isinstance(scoreObj, music21.note.Note):
