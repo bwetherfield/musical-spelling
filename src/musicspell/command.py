15c14553b38ba7f0b5e023877a57f687cb5ccbe9
Ben Wetherfield <bwetherfield@gmail.com>
2017-08-31 18:54:56 -0400

    add Args, Attributes, Returns etc to docstrings in command.py
    google style docstrings

--- src/musicspell/command.py
+++ src/musicspell/command.py
@@ -1,7 +1,31 @@
+"""Object-Oriented implementation of sql commands that are called by databases
+in a Visitor-like way
+
+"""
 from musicspell.db import Db
 
 class Command:
-    """base sql command"""
+    """base sql command
+
+    Arguments:
+        tbl: associated database table
+        *conditions, optional: apply command to rows for which conditions are
+            true
+        **kwargs, optional: change rows with these column, value pairs
+
+    Attributes:
+        _cmdType: overridden attribute
+        cmds: list containing self. This accommodates
+            :method:`musicspell.db.Db.checkCmd` which checks commands contained
+            in its parameter objects
+        tbl: associated database table
+        *conditions, optional: apply command to rows for which conditions are
+            true
+        **kwargs, optional: change rows with these column, value pairs
+        self._data: overridden attribute.
+        self._cmdStr: overridden attribute
+
+    """
 
     _cmdType = None
 
@@ -15,16 +39,28 @@ class Command:
         self._cmdStr = None
 
     def execute(self):
-        """ """
+        """virtual method"""
         raise NotImplementedError
 
 class Insert(Command):
-    """concrete sql command INSERT"""
+    """concrete sql command INSERT. Extends `Command`.
+
+    Attributes:
+        _cmdType: "INSERT". Overrides :attribute:`Command._cmdType`
+
+    """
 
     _cmdType = "INSERT"
 
     def getString(self):
-        """ """
+        """Getter for explicit sql command and parameters
+
+        Overrides `_data` to hold sql paramaters and `_cmdStr` to hold sql command.
+
+        Returns:
+            (str, str): tuple containing sql string and parameters
+
+        """
         if self._cmdStr is None:
             tabStr = ' INTO {}'.format(self.tbl)
             klist, wlist, qlist = [], [], []
@@ -42,14 +78,17 @@ class Insert(Command):
         return self._cmdStr, self._data
 
     def errorCheck(self):
-        """ """
+        """command-specific error checking"""
         if self.kwargs == {}:
             raise ValueError('column, entry pairs needed to amend database')
 
     def execute(self, cursor):
-        """
+        """Execute sql command
 
-        :param cursor: 
+        Implements Visitor-like behavior
+
+        Args:
+            cursor: :obj:`sqlite3.Cursor` from database where the command is executed
 
         """
         self.errorCheck()
@@ -57,12 +96,17 @@ class Insert(Command):
         cursor.execute(cmd, data)
 
 class Select(Command):
-    """concrete sql command SELECT"""
+    """concrete sql command SELECT. Extends `Command`.
+
+    Attributes:
+        _cmdType: "SELECT". Overrides :attribute:`Command._cmdType`
+
+    """
 
     _cmdType = "SELECT"
 
     def getString(self):
-        """ """
+        """Getter for explicit sql command"""
         if self._cmdStr is None:
             cmd = ' * from {}'.format(self.tbl)
             self._cmdStr = cmd
@@ -73,9 +117,17 @@ class Select(Command):
         return self._cmdStr
 
     def execute(self, cursor):
-        """
+        """Execute sql command
 
-        :param cursor: 
+        Implements Visitor-like behavior
+
+        Args:
+            cursor: :obj:`sqlite3.Cursor` from database where the command is executed
+
+        Returns:
+            [:obj:`sqlite3.Row`], optional: rows of the database associated
+                with `cursor` that meet the conditions or None if there are
+                nonesuch
 
         """
         cmd = self.getString()
@@ -85,12 +137,24 @@ class Select(Command):
         else: return rows
 
 class Update(Command):
-    """concrete sql command UPDATE"""
+    """concrete sql command UPDATE. Extends `Command`.
+
+    Attributes:
+        _cmdType: "UPDATE". Overrides :attribute:`Command._cmdType`
+
+    """
 
     _cmdType = "UPDATE"
 
     def getString(self):
-        """ """
+        """Getter for explicit sql command and parameters
+
+        Overrides `_data` to hold sql paramaters and `_cmdStr` to hold sql command.
+
+        Returns:
+            (str, str): tuple containing sql string and parameters
+
+        """
         cmd = ' {}'.format(self.tbl)
         ksubs, wlist = [], []
         for k in self.kwargs.keys():
@@ -107,14 +171,17 @@ class Update(Command):
         return self._cmdStr, self._data
 
     def errorCheck(self):
-        """ """
+        """command-specific error checking"""
         if self.kwargs == {}:
             raise ValueError('column, entry pairs needed to amend database')
 
     def execute(self, cursor):
-        """
+        """Execute sql command
 
-        :param cursor: 
+        Implements Visitor-like behavior
+
+        Args:
+            cursor: :obj:`sqlite3.Cursor` from database where the command is executed
 
         """
         self.errorCheck()
@@ -122,12 +189,17 @@ class Update(Command):
         cursor.execute(cmd, data)
 
 class Delete(Command):
-    """concrete sql command DELETE"""
+    """concrete sql command DELETE. Extends `Command`.
+
+    Attributes:
+        _cmdType: "DELETE". Overrides :attribute:`Command._cmdType`
+
+    """
 
     _cmdType = "DELETE"
 
     def getString(self):
-        """ """
+        """Getter for explicit sql command"""
         cmd = ' from {}'.format(self.tbl)
         self._cmdStr = cmd
         if self.conditions != ():
@@ -137,9 +209,12 @@ class Delete(Command):
         return self._cmdStr
 
     def execute(self, cursor):
-        """
+        """Execute sql command
 
-        :param cursor: 
+        Implements Visitor-like behavior
+
+        Args:
+            cursor: :obj:`sqlite3.Cursor` from database where the command is executed
 
         """
         cmd = self.getString()
